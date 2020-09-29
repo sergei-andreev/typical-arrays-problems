cf4f9df313daac4f498f6b770f2467f34e242146
Sergei Andreev <s.andreev.it@gmail.com>
2020-09-29 09:17:13 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.reduce((sum, num) => num + sum, 0) / array.length;
 }
